fc158ac03e39ffcf8c66c18b400f9c81cd2faefb
Jacob Johansen <jacob.johansen@virginpulse.com>
2020-11-09 10:45:55 -0600

    update gradle plugin build information

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -17,10 +17,18 @@ dependencies {
 gradlePlugin{
     plugins {
         create("avro4kAvroGeneration") {
-            id = "avro4kAvroGeneration"
-            implementationClass = "com.magusdevops.avro4k.gradleplugin.Avro4KPlugin"
+            id = "com.magusdevops.avro4k.gradleplugin.Avro4KAvroGeneration"
+            displayName = "Avro4k Avro Generation"
+            description = "Generate Avro Schemas from Avro4k model classes"
+            implementationClass = "com.magusdevops.avro4k.gradleplugin.Avro4KAvroGenerationPlugin"
         }
     }
+}
+
+pluginBundle {
+    website = "https://github.com/MagusDevOps/avro4k-build-plugins"
+    vcsUrl = "https://github.com/MagusDevOps/avro4k-build-plugins.git"
+    tags = listOf("avro", "avro4k", "generation")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
--- gradle-plugin/src/main/kotlin/com/magusdevops/avro4k/gradleplugin/Avro4KAvroGenerationPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/magusdevops/avro4k/gradleplugin/Avro4KAvroGenerationPlugin.kt
@@ -4,7 +4,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPlugin
 
-class Avro4KPlugin : Plugin<Project>{
+class Avro4KAvroGenerationPlugin : Plugin<Project>{
     override fun apply(project: Project) {
         project.plugins.apply(JavaPlugin::class.java)
 
